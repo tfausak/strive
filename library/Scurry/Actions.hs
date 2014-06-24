db47f1a1ec1183ab22c8494dbc7e960dfddc893d
Taylor Fausak <taylor@fausak.me>
2014-06-23 22:46:50 -0500

    Use qualified import to avoid long import statement

--- library/Scurry/Actions.hs
+++ library/Scurry/Actions.hs
@@ -23,104 +23,101 @@ import           Data.ByteString.Lazy    (toStrict)
 import           Data.Monoid             ((<>))
 import           Scurry.Actions.Internal (get, paginate)
 import           Scurry.Client           (Client)
-import           Scurry.Objects          (AthleteSummary, ClubDetailed,
-                                          ClubSummary, CommentSummary,
-                                          GearDetailed, PhotoSummary,
-                                          ZoneSummary)
+import qualified Scurry.Objects          as Objects
 
 -- | <http://strava.github.io/api/v3/athlete/#get-another-details>
-getAthlete :: Client -> Integer -> IO (Either String AthleteSummary)
+getAthlete :: Client -> Integer -> IO (Either String Objects.AthleteSummary)
 getAthlete client athleteId = get client resource query
   where
     resource = "athletes/" <> show athleteId
     query = []
 
 -- | <http://strava.github.io/api/v3/clubs/#get-details>
-getClub :: Client -> Integer -> IO (Either String ClubDetailed)
+getClub :: Client -> Integer -> IO (Either String Objects.ClubDetailed)
 getClub client clubId = get client resource query
   where
     resource = "clubs/" <> show clubId
     query = []
 
 -- | <http://strava.github.io/api/v3/clubs/#get-members>
-getClubMembers :: Client -> Integer -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getClubMembers :: Client -> Integer -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getClubMembers client clubId page perPage = get client resource query
   where
     resource = "clubs/" <> show clubId <> "/members"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/comments/#list>
-getComments :: Client -> Integer -> Bool -> Integer -> Integer -> IO (Either String [CommentSummary])
+getComments :: Client -> Integer -> Bool -> Integer -> Integer -> IO (Either String [Objects.CommentSummary])
 getComments client activityId includeMarkdown page perPage = get client resource query
   where
     resource = "activities/" <> show activityId <> "/comments"
     query = ("markdown", toStrict (encode includeMarkdown)) : paginate page perPage
 
 -- | <http://strava.github.io/api/v3/follow/#both>
-getCommonFriends :: Client -> Integer -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getCommonFriends :: Client -> Integer -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getCommonFriends client athleteId page perPage = get client resource query
   where
     resource = "/athletes/" <> show athleteId <> "/both-following"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/clubs/#get-athletes>
-getCurrentClubs :: Client -> IO (Either String [ClubSummary])
+getCurrentClubs :: Client -> IO (Either String [Objects.ClubSummary])
 getCurrentClubs client = get client resource query
   where
     resource = "athlete/clubs"
     query = []
 
 -- | <http://strava.github.io/api/v3/follow/#followers>
-getCurrentFollowers :: Client -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getCurrentFollowers :: Client -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getCurrentFollowers client page perPage = get client resource query
   where
     resource = "athlete/followers"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/follow/#friends>
-getCurrentFriends :: Client -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getCurrentFriends :: Client -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getCurrentFriends client page perPage = get client resource query
   where
     resource = "athlete/friends"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/follow/#followers>
-getFollowers :: Client -> Integer -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getFollowers :: Client -> Integer -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getFollowers client athleteId page perPage = get client resource query
   where
     resource = "athletes/" <> show athleteId <> "/followers"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/follow/#friends>
-getFriends :: Client -> Integer -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getFriends :: Client -> Integer -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getFriends client athleteId page perPage = get client resource query
   where
     resource = "athletes/" <> show athleteId <> "/friends"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/gear/#show>
-getGear :: Client -> String -> IO (Either String GearDetailed)
+getGear :: Client -> String -> IO (Either String Objects.GearDetailed)
 getGear client gearId = get client resource query
   where
     resource = "gear/" <> gearId
     query = []
 
 -- | <http://strava.github.io/api/v3/kudos/#list>
-getKudoers :: Client -> Integer -> Integer -> Integer -> IO (Either String [AthleteSummary])
+getKudoers :: Client -> Integer -> Integer -> Integer -> IO (Either String [Objects.AthleteSummary])
 getKudoers client activityId page perPage = get client resource query
   where
     resource = "activities/" <> show activityId <> "/kudos"
     query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/photos/#list>
-getPhotos :: Client -> Integer -> IO (Either String [PhotoSummary])
+getPhotos :: Client -> Integer -> IO (Either String [Objects.PhotoSummary])
 getPhotos client activityId = get client resource query
   where
     resource = "activities/" <> show activityId <> "/photos"
     query = []
 
 -- | <http://strava.github.io/api/v3/activities/#zones>
-getZones :: Client -> Integer -> IO (Either String [ZoneSummary])
+getZones :: Client -> Integer -> IO (Either String [Objects.ZoneSummary])
 getZones client activityId = get client resource query
   where
     resource = "activities/" <> show activityId <> "/zones"
