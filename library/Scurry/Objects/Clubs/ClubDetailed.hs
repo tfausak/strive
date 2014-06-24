746ef8fdb732f46cca15e307cbf5d220c31d99ce
Taylor Fausak <taylor@fausak.me>
2014-06-23 19:35:11 -0500

    Define ClubDetailed type

--- library/Scurry/Objects/Clubs/ClubDetailed.hs
+++ library/Scurry/Objects/Clubs/ClubDetailed.hs
@@ -3,5 +3,21 @@ module Scurry.Objects.Clubs.ClubDetailed
     ( ClubDetailed (..)
     ) where
 
+import           Data.Text (Text)
+
 -- | Detailed club representation.
 data ClubDetailed = ClubDetailed
+    { city          :: Text
+    , club_type     :: Text
+    , country       :: Text
+    , description   :: Text
+    , id            :: Integer
+    , memberCount   :: Integer
+    , name          :: Text
+    , private       :: Bool
+    , profile       :: Text
+    , profileMedium :: Text
+    , resourceState :: Integer
+    , sportType     :: Text
+    , state         :: Text
+    } deriving (Show)
