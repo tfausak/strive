bb346f5a086f3e33ed9d769a95ef52081cf6821c
Taylor Fausak <taylor@fausak.me>
2014-06-25 19:03:07 -0500

    Move friend actions into module

--- library/Strive/Actions.hs
+++ library/Strive/Actions.hs
@@ -3,11 +3,6 @@
 -- | Functions for performing actions against the API.
 module Strive.Actions
     ( getComments
-    , getCommonFriends
-    , getCurrentFollowers
-    , getCurrentFriends
-    , getFollowers
-    , getFriends
     , getKudoers
     , getPhotos
     , module Actions
@@ -20,6 +15,7 @@ import           Strive.Actions.Activities as Actions
 import           Strive.Actions.Athletes   as Actions
 import           Strive.Actions.Clubs      as Actions
 import           Strive.Actions.Efforts    as Actions
+import           Strive.Actions.Friends    as Actions
 import           Strive.Actions.Gear       as Actions
 import           Strive.Actions.Internal   (get, paginate)
 import           Strive.Actions.Segments   as Actions
@@ -33,41 +29,6 @@ getComments client activityId includeMarkdown page perPage = get client resource
   where
     resource = "activities/" <> show activityId <> "/comments"
     query = ("markdown", toStrict (encode includeMarkdown)) : paginate page perPage
-
--- | <http://strava.github.io/api/v3/follow/#both>
-getCommonFriends :: Client -> Types.AthleteId -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
-getCommonFriends client athleteId page perPage = get client resource query
-  where
-    resource = "athletes/" <> show athleteId <> "/both-following"
-    query = paginate page perPage
-
--- | <http://strava.github.io/api/v3/follow/#followers>
-getCurrentFollowers :: Client -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
-getCurrentFollowers client page perPage = get client resource query
-  where
-    resource = "athlete/followers"
-    query = paginate page perPage
-
--- | <http://strava.github.io/api/v3/follow/#friends>
-getCurrentFriends :: Client -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
-getCurrentFriends client page perPage = get client resource query
-  where
-    resource = "athlete/friends"
-    query = paginate page perPage
-
--- | <http://strava.github.io/api/v3/follow/#followers>
-getFollowers :: Client -> Types.AthleteId -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
-getFollowers client athleteId page perPage = get client resource query
-  where
-    resource = "athletes/" <> show athleteId <> "/followers"
-    query = paginate page perPage
-
--- | <http://strava.github.io/api/v3/follow/#friends>
-getFriends :: Client -> Types.AthleteId -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
-getFriends client athleteId page perPage = get client resource query
-  where
-    resource = "athletes/" <> show athleteId <> "/friends"
-    query = paginate page perPage
 
 -- | <http://strava.github.io/api/v3/kudos/#list>
 getKudoers :: Client -> Types.ActivityId -> Types.Page -> Types.PerPage -> IO (Either String [Objects.AthleteSummary])
