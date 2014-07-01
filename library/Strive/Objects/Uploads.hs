da9e074bd24e9a490a709f5727d6b245eb20a082
Taylor Fausak <taylor@fausak.me>
2014-06-30 22:30:49 -0500

    Prefix upload fields and move into single module

--- library/Strive/Objects/Uploads.hs
+++ library/Strive/Objects/Uploads.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | <http://strava.github.io/api/v3/uploads/>
 module Strive.Objects.Uploads
-    ( module Strive.Objects.Uploads.UploadDetailed
+    ( UploadDetailed (..)
     ) where
 
-import Strive.Objects.Uploads.UploadDetailed (UploadDetailed)
+import Control.Applicative (empty, (<$>), (<*>))
+import Data.Aeson (FromJSON, Value (Object), parseJSON, (.:), (.:?))
+import Data.Text (Text)
+
+-- | <http://strava.github.io/api/v3/uploads/#attributes>
+data UploadDetailed = UploadDetailed
+    { uploadDetailedActivityId :: Maybe Integer
+    , uploadDetailedError      :: Maybe Text
+    , uploadDetailedExternalId :: Text
+    , uploadDetailedId         :: Integer
+    , uploadDetailedStatus     :: Text
+    } deriving Show
+
+instance FromJSON UploadDetailed where
+    parseJSON (Object o) = UploadDetailed
+        <$> o .:? "activity_id"
+        <*> o .:? "error"
+        <*> o .: "external_id"
+        <*> o .: "id"
+        <*> o .: "status"
+    parseJSON _ = empty
