6da28ee5db5579d1db8a480464a5f7f46a5aa336
Taylor Fausak <taylor@fausak.me>
2014-06-30 22:07:35 -0500

    Prefix comment fields and move into single module

--- library/Strive/Objects/Comments.hs
+++ library/Strive/Objects/Comments.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | <http://strava.github.io/api/v3/comments/>
 module Strive.Objects.Comments
-    ( module Strive.Objects.Comments.CommentSummary
+    ( CommentSummary (..)
     ) where
 
-import Strive.Objects.Comments.CommentSummary (CommentSummary)
+import Control.Applicative (empty, (<$>), (<*>))
+import Data.Aeson (FromJSON, Value (Object), parseJSON, (.:))
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+import Strive.Objects.Athletes (AthleteSummary)
+
+-- | <http://strava.github.io/api/v3/comments/#summary-and-detailed-representation-attributes>
+data CommentSummary = CommentSummary
+    { commentSummaryActivityId    :: Integer
+    , commentSummaryAthlete       :: AthleteSummary
+    , commentSummaryCreatedAt     :: UTCTime
+    , commentSummaryId            :: Integer
+    , commentSummaryResourceState :: Integer
+    , commentSummaryText          :: Text
+    } deriving Show
+
+instance FromJSON CommentSummary where
+    parseJSON (Object o) = CommentSummary
+        <$> o .: "activity_id"
+        <*> o .: "athlete"
+        <*> o .: "created_at"
+        <*> o .: "id"
+        <*> o .: "resource_state"
+        <*> o .: "text"
+    parseJSON _ = empty
