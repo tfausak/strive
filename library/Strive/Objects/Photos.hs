a49b1fc9ea99f56c62ef72b460ce3d4608709b35
Taylor Fausak <taylor@fausak.me>
2014-06-30 22:10:25 -0500

    Prefix photo fields and move into single module

--- library/Strive/Objects/Photos.hs
+++ library/Strive/Objects/Photos.hs
@@ -1,6 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | <http://strava.github.io/api/v3/photos/>
 module Strive.Objects.Photos
-    ( module Strive.Objects.Photos.PhotoSummary
+    ( PhotoSummary (..)
     ) where
 
-import Strive.Objects.Photos.PhotoSummary (PhotoSummary)
+import Control.Applicative (empty, (<$>), (<*>))
+import Data.Aeson (FromJSON, Value (Object), parseJSON, (.:), (.:?))
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+
+-- | <http://strava.github.io/api/v3/photos/#summary-and-detailed-representation-attributes>
+data PhotoSummary = PhotoSummary
+    { photoSummaryActivityId    :: Integer
+    , photoSummaryCaption       :: Text
+    , photoSummaryCreatedAt     :: UTCTime
+    , photoSummaryId            :: Integer
+    , photoSummaryLocation      :: Maybe (Double, Double)
+    , photoSummaryRef           :: Text
+    , photoSummaryResourceState :: Integer
+    , photoSummaryType          :: Text
+    , photoSummaryUid           :: Text
+    , photoSummaryUploadedAt    :: UTCTime
+    } deriving Show
+
+instance FromJSON PhotoSummary where
+    parseJSON (Object o) = PhotoSummary
+        <$> o .: "activity_id"
+        <*> o .: "caption"
+        <*> o .: "created_at"
+        <*> o .: "id"
+        <*> o .:? "location"
+        <*> o .: "ref"
+        <*> o .: "resource_state"
+        <*> o .: "type"
+        <*> o .: "uid"
+        <*> o .: "uploaded_at"
+    parseJSON _ = empty
