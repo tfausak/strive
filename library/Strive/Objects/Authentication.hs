e0ccbdd2605948eb250d7f5d5886d426dd316d4f
Taylor Fausak <taylor@fausak.me>
2014-06-30 22:45:29 -0500

    Prefix authorization fields and move into single module

--- library/Strive/Objects/Authentication.hs
+++ library/Strive/Objects/Authentication.hs
@@ -1,8 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | <http://strava.github.io/api/v3/oauth/>
 module Strive.Objects.Authentication
-    ( module Strive.Objects.Authentication.DeauthorizationResponse
-    , module Strive.Objects.Authentication.TokenExchangeResponse
+    ( TokenExchangeResponse (..)
+    , DeauthorizationResponse (..)
     ) where
 
-import Strive.Objects.Authentication.DeauthorizationResponse (DeauthorizationResponse)
-import Strive.Objects.Authentication.TokenExchangeResponse (TokenExchangeResponse)
+import Control.Applicative (empty, (<$>), (<*>))
+import Data.Aeson (FromJSON, Value (Object), parseJSON, (.:))
+import Data.Text (Text)
+import Strive.Objects.Athletes (AthleteDetailed)
+
+-- | <http://strava.github.io/api/v3/oauth/#example-response>
+data TokenExchangeResponse = TokenExchangeResponse
+    { tokenExchangeResponseAccessToken :: Text
+    , tokenExchangeResponseAthlete     :: AthleteDetailed
+    } deriving Show
+
+instance FromJSON TokenExchangeResponse where
+    parseJSON (Object o) = TokenExchangeResponse
+        <$> o .: "access_token"
+        <*> o .: "athlete"
+    parseJSON _ = empty
+
+-- | <http://strava.github.io/api/v3/oauth/#example-response-1>
+data DeauthorizationResponse = DeauthorizationResponse
+    { deauthorizationResponseAccessToken :: Text
+    } deriving Show
+
+instance FromJSON DeauthorizationResponse where
+    parseJSON (Object o) = DeauthorizationResponse
+        <$> o .: "access_token"
+    parseJSON _ = empty
