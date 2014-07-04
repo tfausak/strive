0acd6269d7a63e23ce9a2aa40f7207283a6a75cc
Taylor Fausak <taylor@fausak.me>
2014-07-04 08:39:06 -0500

    Implement IdLens class and instance for AthleteMeta
    This works, but manually writing all of the classes and instances would
    be a chore.

--- library/Strive/Lenses.hs
+++ library/Strive/Lenses.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+
 -- | Lenses for easily getting and setting values.
-module Strive.Lenses
-  ( Lens
-  , get
-  , set
-  ) where
+module Strive.Lenses where
+
+import Strive.Objects
 
 -- | A lens for a record, returning a field and a residual.
 type Lens a b = a -> (b, b -> a)
@@ -15,3 +16,12 @@ get = (fst .)
 -- | Set a field in a record using a lens.
 set :: Lens a b -> b -> a -> a
 set = flip . (snd .)
+
+class IdLens a b | a -> b where
+  id :: Lens a b
+
+instance IdLens AthleteMeta Integer where
+  id athlete =
+    ( athleteMetaId athlete
+    , \ id' -> athlete { athleteMetaId = id' }
+    )
