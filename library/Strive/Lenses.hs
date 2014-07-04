8cf988a0b84eb3a30a16982ffe88554e4614c299
Taylor Fausak <taylor@fausak.me>
2014-07-04 08:28:35 -0500

    Implement Lens type alias and get/set

--- library/Strive/Lenses.hs
+++ library/Strive/Lenses.hs
@@ -1,3 +1,17 @@
+-- | Lenses for easily getting and setting values.
 module Strive.Lenses
-  (
+  ( Lens
+  , get
+  , set
   ) where
+
+-- | A lens for a record, returning a field and a residual.
+type Lens a b = a -> (b, b -> a)
+
+-- | Get a field from a record using a lens.
+get :: Lens a b -> a -> b
+get = (fst .)
+
+-- | Set a field in a record using a lens.
+set :: Lens a b -> b -> a -> a
+set = flip . (snd .)
