13b9ec1ee7294eb1610a7ee626ce944e7889c6a5
Taylor Fausak <taylor@fausak.me>
2014-07-05 15:41:03 -0500

    Write how the internals of buildAuthorizeUrl should work

--- library/Strive.hs
+++ library/Strive.hs
@@ -1,4 +1,6 @@
 -- | A Haskell client for the <http://strava.github.io/api/ Strava V3 API>.
 module Strive
-  (
+  ( module Strive.Actions
   ) where
+
+import Strive.Actions
---
+++ library/Strive/Actions.hs
@@ -0,0 +1,20 @@
+-- | Functions for performing actions against the API.
+module Strive.Actions where
+
+import Data.ByteString.Char8 (unpack)
+import Data.Monoid ((<>))
+import Network.HTTP.Types (renderQuery, toQuery)
+-- import Strive.Options (BuildAuthorizeUrlOptions)
+
+-- | * Authentication
+
+-- | <http://strava.github.io/api/v3/oauth/#get-authorize>
+buildAuthorizeUrl :: Integer -> String -> BuildAuthorizeUrlOptions -> String
+buildAuthorizeUrl clientId redirectUrl options =
+  "https://www.strava.com/oauth/authorize" <> unpack (renderQuery True query)
+ where
+  query = toQuery
+    [ ("client_id", show clientId)
+    , ("redirect_url", redirectUrl)
+    , ("response_type", "code")
+    ] <> toQuery options
