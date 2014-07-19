5e05aaab6b81cded6430183d4229751134ca0239
Taylor Fausak <taylor@fausak.me>
2014-07-19 12:20:19 -0500

    Clean up enums
    I sorted everything by name and made sure all FromJSON instances were
    total.

--- library/Strive/Enums.hs
+++ library/Strive/Enums.hs
@@ -1,27 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Types for choosing an option from a limited set.
-module Strive.Enums where
+module Strive.Enums
+  ( ActivityType (..)
+  , ActivityZoneType (..)
+  , AgeGroup (..)
+  , ClubType (..)
+  , FrameType (..)
+  , Gender (..)
+  , MeasurementPreference (..)
+  , PhotoType (..)
+  , Resolution (..)
+  , ResourceState (..)
+  , SegmentActivityType (..)
+  , SeriesType (..)
+  , SportType (..)
+  , WeightClass (..)
+  ) where
 
 import Control.Applicative (empty)
 import Data.Aeson (FromJSON, Value (Number, String), parseJSON)
 import Data.Char (toLower, toUpper)
 import Data.Text (unpack)
 import Text.Read (readMaybe)
-
--- | An athlete's gender.
-data Gender
-  = Female
-  | Male
-
-instance Show Gender where
-  show Female = "F"
-  show Male   = "M"
-
-instance FromJSON Gender where
-  parseJSON (String "F") = return Female
-  parseJSON (String "M") = return Male
-  parseJSON _ = empty
 
 -- | An activity's type.
 data ActivityType
@@ -62,6 +63,17 @@ instance FromJSON ActivityType where
   parseJSON (String "workout")        = return Workout
   parseJSON _ = empty
 
+-- | An activity zone's type.
+data ActivityZoneType
+  = HeartrateZone
+  | PowerZone
+  deriving Show
+
+instance FromJSON ActivityZoneType where
+  parseJSON (String "heartrate") = return HeartrateZone
+  parseJSON (String "power")     = return PowerZone
+  parseJSON _ = empty
+
 -- | An athlete's age group.
 data AgeGroup
   = Ages0To24
@@ -79,43 +91,71 @@ instance Show AgeGroup where
   show Ages55To64 = "55_64"
   show Ages65Plus = "65_plus"
 
--- | An athlete's weight class.
-data WeightClass
-  = Pounds0To124
-  | Pounds125To149
-  | Pounds150To164
-  | Pounds165To179
-  | Pounds180To199
-  | Pounds200Plus
-  | Kilograms0To54
-  | Kilograms55To64
-  | Kilograms65To74
-  | Kilograms75To84
-  | Kilograms85To94
-  | Kilograms95Plus
+-- | A club's type.
+data ClubType
+  = CasualClub
+  | Company
+  | Other
+  | RacingTeam
+  | Shop
+  deriving Show
 
-instance Show WeightClass where
-  show Pounds0To124    = "0_124"
-  show Pounds125To149  = "125_149"
-  show Pounds150To164  = "150_164"
-  show Pounds165To179  = "165_179"
-  show Pounds180To199  = "180_199"
-  show Pounds200Plus   = "200_plus"
-  show Kilograms0To54  = "0_54"
-  show Kilograms55To64 = "55_64"
-  show Kilograms65To74 = "65_74"
-  show Kilograms75To84 = "75_84"
-  show Kilograms85To94 = "85_94"
-  show Kilograms95Plus = "95_plus"
+instance FromJSON ClubType where
+  parseJSON (String "casual_club") = return CasualClub
+  parseJSON (String "company")     = return Company
+  parseJSON (String "other")       = return Other
+  parseJSON (String "racing_team") = return RacingTeam
+  parseJSON (String "shop")        = return Shop
+  parseJSON _ = empty
 
--- | A segment's activity type.
-data SegmentActivityType
-  = Riding
-  | Running
+-- | A bike's frame type.
+data FrameType
+  = CrossFrame
+  | MountainFrame
+  | RoadFrame
+  | TimeTrialFrame
+  deriving Show
 
-instance Show SegmentActivityType where
-  show Riding  = "riding"
-  show Running = "running"
+instance FromJSON FrameType where
+  parseJSON (Number 2) = return CrossFrame
+  parseJSON (Number 1) = return MountainFrame
+  parseJSON (Number 3) = return RoadFrame
+  parseJSON (Number 4) = return TimeTrialFrame
+  parseJSON _ = empty
+
+-- | An athlete's gender.
+data Gender
+  = Female
+  | Male
+
+instance Show Gender where
+  show Female = "F"
+  show Male   = "M"
+
+instance FromJSON Gender where
+  parseJSON (String "F") = return Female
+  parseJSON (String "M") = return Male
+  parseJSON _ = empty
+
+-- | An athlete's measurement preference.
+data MeasurementPreference
+  = Feet
+  | Meters
+  deriving Show
+
+instance FromJSON MeasurementPreference where
+  parseJSON (String "feet")   = return Feet
+  parseJSON (String "meters") = return Meters
+  parseJSON _ = empty
+
+-- | A photo's type.
+data PhotoType
+  = InstagramPhoto
+  deriving Show
+
+instance FromJSON PhotoType where
+  parseJSON (String "InstagramPhoto") = return InstagramPhoto
+  parseJSON _ = empty
 
 -- | A stream's resolution.
 data Resolution
@@ -134,6 +174,28 @@ instance FromJSON Resolution where
   parseJSON (String "high")   = return High
   parseJSON _ = empty
 
+-- | A resource's state.
+data ResourceState
+  = Meta
+  | Summary
+  | Detailed
+  deriving Show
+
+instance FromJSON ResourceState where
+  parseJSON (Number 1) = return Meta
+  parseJSON (Number 2) = return Summary
+  parseJSON (Number 3) = return Detailed
+  parseJSON _ = empty
+
+-- | A segment's activity type.
+data SegmentActivityType
+  = Riding
+  | Running
+
+instance Show SegmentActivityType where
+  show Riding  = "riding"
+  show Running = "running"
+
 -- | A series' type in a stream.
 data SeriesType
   = Distance
@@ -148,92 +210,46 @@ instance FromJSON SeriesType where
   parseJSON (String "time")     = return Time
   parseJSON _ = empty
 
--- | A resource's state.
-data ResourceState
-  = Meta
-  | Summary
-  | Detailed
-  deriving Show
-
-instance FromJSON ResourceState where
-  parseJSON (Number 1) = return Meta
-  parseJSON (Number 2) = return Summary
-  parseJSON (Number 3) = return Detailed
-  parseJSON _ = empty
-
--- | An athlete's measurement preference.
-data MeasurementPreference
-  = Feet
-  | Meters
-  deriving Show
-
-instance FromJSON MeasurementPreference where
-  parseJSON (String "feet")   = return Feet
-  parseJSON (String "meters") = return Meters
-  parseJSON _ = empty
-
--- | An activity zone's type.
-data ActivityZoneType
-  = HeartrateZone
-  | PowerZone
-  deriving Show
-
-instance FromJSON ActivityZoneType where
-  parseJSON (String "heartrate") = return HeartrateZone
-  parseJSON (String "power")     = return PowerZone
-
--- | A photo's type.
-data PhotoType
-  = InstagramPhoto
-  deriving Show
-
-instance FromJSON PhotoType where
-  parseJSON (String "InstagramPhoto") = return InstagramPhoto
-  parseJSON _ = empty
-
--- | A club's type.
-data ClubType
-  = CasualClub
-  | RacingTeam
-  | Shop
-  | Company
-  | Other
-  deriving Show
-
-instance FromJSON ClubType where
-  parseJSON (String "casual_club") = return CasualClub
-  parseJSON (String "racing_team") = return RacingTeam
-  parseJSON (String "shop")        = return Shop
-  parseJSON (String "company")     = return Company
-  parseJSON (String "other")       = return Other
-  parseJSON _ = empty
-
 -- | A club's sport type.
 data SportType
   = SportCycling
+  | SportOther
   | SportRunning
   | SportTriathalon
-  | SportOther
   deriving Show
 
 instance FromJSON SportType where
   parseJSON (String "cycling")    = return SportCycling
+  parseJSON (String "other")      = return SportOther
   parseJSON (String "running")    = return SportRunning
   parseJSON (String "triathalon") = return SportTriathalon
-  parseJSON (String "other")      = return SportOther
   parseJSON _ = empty
 
--- | A bike's frame type.
-data FrameType
-  = MountainFrame
-  | CrossFrame
-  | RoadFrame
-  | TimeTrialFrame
-  deriving Show
+-- | An athlete's weight class.
+data WeightClass
+  = Kilograms0To54
+  | Kilograms55To64
+  | Kilograms65To74
+  | Kilograms75To84
+  | Kilograms85To94
+  | Kilograms95Plus
+  | Pounds0To124
+  | Pounds125To149
+  | Pounds150To164
+  | Pounds165To179
+  | Pounds180To199
+  | Pounds200Plus
 
-instance FromJSON FrameType where
-  parseJSON (Number 1) = return MountainFrame
-  parseJSON (Number 2) = return CrossFrame
-  parseJSON (Number 3) = return RoadFrame
-  parseJSON (Number 4) = return TimeTrialFrame
-  parseJSON _ = empty
+instance Show WeightClass where
+  show Kilograms0To54  = "0_54"
+  show Kilograms55To64 = "55_64"
+  show Kilograms65To74 = "65_74"
+  show Kilograms75To84 = "75_84"
+  show Kilograms85To94 = "85_94"
+  show Kilograms95Plus = "95_plus"
+  show Pounds0To124    = "0_124"
+  show Pounds125To149  = "125_149"
+  show Pounds150To164  = "150_164"
+  show Pounds165To179  = "165_179"
+  show Pounds180To199  = "180_199"
+  show Pounds200Plus   = "200_plus"
