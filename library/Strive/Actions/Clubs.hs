c6fbd35630abfcb363bc76d76c414775a280b4ed
Taylor Fausak <taylor@fausak.me>
2015-01-17 10:13:56 -0600

    Re #84; add support for joining clubs

--- library/Strive/Actions/Clubs.hs
+++ library/Strive/Actions/Clubs.hs
@@ -4,11 +4,15 @@ module Strive.Actions.Clubs
   , getCurrentClubs
   , getClubMembers
   , getClubActivities
+  , joinClub
   ) where
 
-import Network.HTTP.Types (Query, toQuery)
+import Data.ByteString.Char8 (unpack)
+import Data.ByteString.Lazy (toStrict)
+import Network.HTTP.Conduit (responseBody, responseStatus)
+import Network.HTTP.Types (Query, methodPost, ok200, toQuery)
 import Strive.Client (Client)
-import Strive.Internal.HTTP (get)
+import Strive.Internal.HTTP (buildRequest, get, performRequest)
 import Strive.Options (GetClubActivitiesOptions, GetClubMembersOptions)
 import Strive.Types (ActivitySummary, AthleteSummary, ClubDetailed,
                      ClubSummary)
@@ -40,3 +44,15 @@ getClubActivities client clubId options = get client resource query
  where
   resource = "api/v3/clubs/" ++ show clubId ++ "/activities"
   query = toQuery options
+
+-- | <http://strava.github.io/api/v3/clubs/#join>
+joinClub :: Client -> Integer -> IO (Either String ())
+joinClub client clubId = do
+  request <- buildRequest methodPost client resource query
+  response <- performRequest client request
+  return (if responseStatus response == ok200
+    then Right ()
+    else Left ((unpack . toStrict . responseBody) response))
+ where
+  resource = "api/v3/clubs/" ++ show clubId ++ "/join"
+  query = [] :: Query
