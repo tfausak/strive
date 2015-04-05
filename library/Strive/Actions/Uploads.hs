11191d7739c8c41822cc5cdd5a597772198a899f
Taylor Fausak <taylor@fausak.me>
2015-04-05 18:18:20 -0500

    Create aliases for uploads

--- library/Strive/Actions/Uploads.hs
+++ library/Strive/Actions/Uploads.hs
@@ -13,8 +13,12 @@ import Strive.Internal.HTTP (buildRequest, decodeValue, get, performRequest)
 import Strive.Options (UploadActivityOptions)
 import Strive.Types (UploadStatus)
 
+-- TODO: Move to Strive.Aliases
+type Extension = String
+type UploadId = Integer
+
 -- | <http://strava.github.io/api/v3/uploads/#post-file>
-uploadActivity :: Client -> ByteString -> String -> UploadActivityOptions -> Result UploadStatus
+uploadActivity :: Client -> ByteString -> Extension -> UploadActivityOptions -> Result UploadStatus
 uploadActivity client body dataType options = do
   initialRequest <- buildRequest methodPost client resource query
   let request = initialRequest
@@ -29,7 +33,7 @@ uploadActivity client body dataType options = do
     ] ++ toQuery options
 
 -- | <http://strava.github.io/api/v3/uploads/#get-status>
-getUpload :: Client -> Integer -> Result UploadStatus
+getUpload :: Client -> UploadId -> Result UploadStatus
 getUpload client uploadId = get client resource query
  where
   resource = "api/v3/uploads/" ++ show uploadId
