cd58a77db5902b98fb0f3b8ea0e35aea8a353f2c
Taylor Fausak <taylor@fausak.me>
2015-06-05 23:10:24 -0500

    Change stream helpers to take a single stream

--- library/Strive/Utilities.hs
+++ library/Strive/Utilities.hs
@@ -14,9 +14,9 @@ module Strive.Utilities
   , with
   ) where
 
-import Data.Aeson (Result (..), FromJSON (..), fromJSON)
+import Data.Aeson (FromJSON, Result (Error, Success), fromJSON)
 import Data.Default (Default, def)
-import Data.List (find)
+import Data.Maybe (mapMaybe)
 import Data.Text (pack)
 import Strive.Enums (StreamType (..))
 import Strive.Types (StreamDetailed (..))
@@ -25,43 +25,45 @@ import Strive.Types (StreamDetailed (..))
 with :: Default a => [a -> a] -> a
 with = foldr ($) def
 
-altitudeStream :: [StreamDetailed] -> Maybe [Double]
+altitudeStream :: StreamDetailed -> Maybe [Double]
 altitudeStream = lookupStream AltitudeStream
 
-cadenceStream :: [StreamDetailed] -> Maybe [Integer]
+cadenceStream :: StreamDetailed -> Maybe [Integer]
 cadenceStream = lookupStream CadenceStream
 
-distanceStream :: [StreamDetailed] -> Maybe [Double]
+distanceStream :: StreamDetailed -> Maybe [Double]
 distanceStream = lookupStream DistanceStream
 
-gradeSmoothStream :: [StreamDetailed] -> Maybe [Double]
+gradeSmoothStream :: StreamDetailed -> Maybe [Double]
 gradeSmoothStream = lookupStream GradeSmoothStream
 
-heartrateStream :: [StreamDetailed] -> Maybe [Integer]
+heartrateStream :: StreamDetailed -> Maybe [Integer]
 heartrateStream = lookupStream HeartrateStream
 
-latlngStream :: [StreamDetailed] -> Maybe [(Double, Double)]
+latlngStream :: StreamDetailed -> Maybe [(Double, Double)]
 latlngStream = lookupStream LatlngStream
 
-movingStream :: [StreamDetailed] -> Maybe [Bool]
+movingStream :: StreamDetailed -> Maybe [Bool]
 movingStream = lookupStream MovingStream
 
-tempStream :: [StreamDetailed] -> Maybe [Integer]
+tempStream :: StreamDetailed -> Maybe [Integer]
 tempStream = lookupStream TempStream
 
-timeStream :: [StreamDetailed] -> Maybe [Integer]
+timeStream :: StreamDetailed -> Maybe [Integer]
 timeStream = lookupStream TimeStream
 
-velocitySmoothStream :: [StreamDetailed] -> Maybe [Double]
+velocitySmoothStream :: StreamDetailed -> Maybe [Double]
 velocitySmoothStream = lookupStream VelocitySmoothStream
 
-wattsStream :: [StreamDetailed] -> Maybe [Integer]
+wattsStream :: StreamDetailed -> Maybe [Integer]
 wattsStream = lookupStream WattsStream
 
-lookupStream :: FromJSON a => StreamType -> [StreamDetailed] -> Maybe [a]
-lookupStream stream_type streams =
-  let resultToMaybe (Success a) = Just a
-      resultToMaybe (Error _)   = Nothing
-  in do stream <- find (\stream ->
-            (streamDetailed_type stream) == (pack $ show stream_type)) streams
-        mapM (resultToMaybe . fromJSON) (streamDetailed_data stream)
+lookupStream :: FromJSON a => StreamType -> StreamDetailed -> Maybe [a]
+lookupStream streamType stream =
+  if streamDetailed_type stream == pack (show streamType)
+    then Just (mapMaybe f (streamDetailed_data stream))
+    else Nothing
+ where
+  f value = case fromJSON value of
+    Success x -> Just x
+    Error _ -> Nothing
