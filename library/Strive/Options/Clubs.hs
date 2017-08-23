950f7aed77ac67c3afe6eb3e2981b33e5d226c30
Erlend Hamberg <erlend@hamberg.no>
2017-08-23 18:47:57 +0200

    Create `GetClubActivitiesOptions` type
    The "List club activities" call supports a `before` parameter in
    addition to the pagination parameters:
    http://strava.github.io/api/v3/clubs/#get-activities

--- library/Strive/Options/Clubs.hs
+++ library/Strive/Options/Clubs.hs
@@ -1,13 +1,35 @@
 -- | 'Strive.Actions.Clubs'
 module Strive.Options.Clubs
   ( GetClubMembersOptions
-  , GetClubActivitiesOptions
+  , GetClubActivitiesOptions (..)
   ) where
 
+import Data.Default (Default, def)
+import Data.Time.Clock (UTCTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import Network.HTTP.Types (QueryLike, toQuery)
 import Strive.Internal.Options (PaginationOptions)
 
 -- | 'Strive.Actions.getClubMembers'
 type GetClubMembersOptions = PaginationOptions
 
 -- | 'Strive.Actions.getClubActivities'
-type GetClubActivitiesOptions = PaginationOptions
+data GetClubActivitiesOptions = GetClubActivitiesOptions
+  { getClubActivitiesOptions_before  :: Maybe UTCTime
+  , getClubActivitiesOptions_page    :: Integer
+  , getClubActivitiesOptions_perPage :: Integer
+  } deriving Show
+
+instance Default GetClubActivitiesOptions where
+  def = GetClubActivitiesOptions
+    { getClubActivitiesOptions_before = Nothing
+    , getClubActivitiesOptions_page = 1
+    , getClubActivitiesOptions_perPage = 200
+    }
+
+instance QueryLike GetClubActivitiesOptions where
+  toQuery options = toQuery
+    [ ("before", fmap (show . utcTimeToPOSIXSeconds) (getClubActivitiesOptions_before options))
+    , ("page", Just (show (getClubActivitiesOptions_page options)))
+    , ("per_page", Just (show (getClubActivitiesOptions_perPage options)))
+    ]
